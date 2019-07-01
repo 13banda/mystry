04dd23055ec2ff4bf70e05dbf4762861d661a1ab
Sandeep Singh <wwaheguru9509088985@gmail.com>
2019-07-01 10:48:28 +0530

    update

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const fsExtra = require('fs-extra')
 let currentPath = process.cwd();
 let outputDirname = "formated"
 let resolvePath = path.resolve(process.cwd());
+var spinner = new Spinner();
+
 program
   .version('0.1.0')
   .description('formating the CSV files data')
@@ -23,8 +25,7 @@ program
   .alias('f')
   .description('formating the csv files')
   .option('-o, --outputDirname [type]', 'Add the specified outputDiractory name like [resolve]', outputDirname)
-  .action(function(options){
-    var spinner = new Spinner();
+  .action(async (options) => {
     spinner.setSpinnerString(3)
     spinner.setSpinnerDelay(200)
     spinner.start()
@@ -34,29 +35,26 @@ program
       allFiles.push(currentPath+path.sep+file);
     });
     try {
-      (async ()=> {
         let p = path.resolve(resolvePath,options.outputDirname)
         console.log("reolved output directory :- ",p);
         console.log("Toatal Files :- ", allFiles.length);
         await fsExtra.emptyDir(p)
         for (var i = 0; i < allFiles.length; i++) {
           if(path.extname(allFiles[i]) === ".csv") {
-            spinner.setSpinnerTitle(path.basename(allFiles[i]));
-            let d = await parseCSV(allFiles[i])
+            spinner.setSpinnerTitle(`file index: ${i+1} ,parsing ${path.basename(allFiles[i])} file`);
+            let d = await parseCSV(allFiles[i],(i+1),path.basename(allFiles[i]))
+            spinner.setSpinnerTitle(`file index: ${i+1} ,formating ${path.basename(allFiles[i])} file`);
             d = findPincode(d);
             let r = p+path.sep+path.basename(allFiles[i]);
+            spinner.setSpinnerTitle(`file index: ${i+1} ,saving formated ${path.basename(allFiles[i])} file`);
             await exportCSV(r,d)
           }
         }
         spinner.stop()
         console.log("all Files Done");
         process.exit(-1)
-      })()
-
     } catch (e) {
-
-    } finally {
-
+        console.log(e);
     }
   })
 
@@ -71,7 +69,7 @@ program
          allFiles.push(currentPath+path.sep+file);
        });
 
-       console.log('Total mystry Files ',allFiles.length);
+       console.log('Total CSV Files ',allFiles.length);
   })
  program.on('command:*', function () {
   console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '));
@@ -85,25 +83,29 @@ program.parse(process.argv);
 
 function findPincode (data) {
  let d = data.map((item,index) => {
-   let r = ""
-   let e = 17;
-   for(var i = 1; i<= 17 ; i++){
-     r = r +` ${data[index]["Address"+i]}`
-     delete data[index]["Address"+i]
-   }
+    let r = ""
+    let e = 17;
+     for(var i = 1; i<= 17 ; i++){
+       r = r +` ${data[index]["Address"+i]}`
+       delete data[index]["Address"+i]
+     }
 
-  item["PINCODE"] = r.match(/([0-9]{6})/g) ? r.match(/([0-9]{6})/g)[0] : ""
-  r = r.split(' ').filter(function(currentItem,l,allItems){
-      return (l == allItems.indexOf(currentItem));
-  });
-  r = r.join(" ")
-  r = r.replace('NULL', '').trim()
-  if(r.match(/([0-9]{6})/g)){
-     r = r.replace(new RegExp(r.match(/([0-9]{6})/g)[0], "ig"),"").trim()
-   }
-  item["Address"] = r;
-   return item;
- })
+    item["PINCODE"] = r.match(/([0-9]{6})/g) ? r.match(/([0-9]{6})/g)[0] : ""
+    r = r.split(' ').filter(function(currentItem,l,allItems){
+        return (l == allItems.indexOf(currentItem));
+    });
+    r = r.join(" ")
+    r = r.replace('NULL', '').trim()
+    r = r.replace('#VALUE!', '').trim()
+    if(r.match(/([0-9]{6})/g)){
+       r = r.replace(new RegExp(r.match(/([0-9]{6})/g)[0], "ig"),"").trim()
+     }
+    item["Address"] = r;
+     return item;
+   })
+   d.sort(function(a, b) {
+     return parseInt(a["PINCODE"],10) - parseInt(b["PINCODE"],10);
+   });
  return d
 }
 
@@ -117,20 +119,20 @@ function exportCSV(path,data) {
   });
 }
 
-function parseCSV(filePath) {
+function parseCSV(filePath,index,filename) {
   return new Promise(function(resolve, reject) {
     let i = []
     fs.createReadStream(filePath)
     .pipe(csv.parse({ignoreEmpty: true }))
     .on("data", function(data){
        i.push(data)
+       spinner.setSpinnerTitle(`file index: ${index} ,parsing ${filename} file, row-parsed:${i.length}`);
      })
      .on("end", ()=>{
-       let globalData = []
-        i.forEach((e,index)=>{
+       spinner.setSpinnerTitle(`file index: ${index} ,parsing ${filename} file, loading...`);
+        i = i.map((e,index)=>{
           if(index > 0){
-            let a = {key:globalData.length+index+1}
-            a.sent = 'false'
+            let a = {}
             for(var l=0;l<e.length;l++){
                 if(!e[l]){
                   e[l] = ""
@@ -139,12 +141,11 @@ function parseCSV(filePath) {
             e.forEach((item,index)=>{
               a[(i[0][index])] = item
             })
-            a.key = globalData.length+1;
-              globalData.push(a)
+            return a;
           }
        })
-       i = []
-       resolve(globalData)
+       spinner.setSpinnerTitle(`file index: ${index} ,parsing ${filename} file, done!`);
+       resolve(i)
      });
   });
 }
